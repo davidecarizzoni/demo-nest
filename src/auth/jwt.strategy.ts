e649c41d1b7b49843337e12bceaff12ede068882
davidecarizzoni <davide.carizzoni@runelab.it>
2021-08-03 22:01:41 +0200

    chore: secret key

--- src/auth/jwt.strategy.ts
+++ src/auth/jwt.strategy.ts
@@ -9,7 +9,7 @@ export class JwtStrategy extends PassportStrategy(Strategy) {
   constructor(private authService: AuthService) {
     super({
       jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
-      secretOrKey: 'process.env.SECRET_KEY',
+      secretOrKey: process.env.SECRET_KEY,
     });
   }
 
